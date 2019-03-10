cd27c094535cced397a7712b16aafe6f63f3b12f
noncover222kdy <h.shareworld.y81d@icloud.com>
2019-03-10 18:50:42 +0900

    test

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,9 @@
 FactoryBot.define do
-  factory :message do
-    content {Faker::Lorem.sentence}
-    image {File.open("#{Rails.root}/public/images/no_image.jpg")}
-    user
-    group
+  factory :user do
+    password = Faker::Internet.password(8)
+    name {Faker::Name.last_name}
+    email {Faker::Internet.free_email}
+    password {password}
+    password_confirmation {password}
   end
 end
