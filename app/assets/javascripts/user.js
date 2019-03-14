4c74d614efcc4fcd8c8f3489bcc27831fa1b08b6
noncover222kdy <h.shareworld.y81d@icloud.com>
2019-03-14 21:59:40 +0900

    ユーザー検索　インクリメンタルサーチ実装

---
+++ app/assets/javascripts/user.js
@@ -0,0 +1,43 @@
+$(function() {
+
+var search_list = $("#user-search-result");
+
+function appendUser(user) {
+  var html = `<p class="chat-group-user__name">"${ user.name }"</p>
+              <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${ user.id }" data-user-name=>追加</a>`
+
+  search_list.append(html);
+}
+
+function appendNoUserToHTML(no_user) {
+  var html = `<div class="chat-group-user clearfix">${no_user}
+              </div>`
+  search_list.append(html);
+}
+
+  $("#user-search-field").on("keyup", function() {
+    var input = $("#user-search-field").val();
+    $.ajax({
+      url: '/users',
+      type: 'GET',
+      data: { keyword: input },
+      datatype: 'json'
+    })
+
+    .done(function(users) {
+      console.log(1)
+      // $("#user-search-result").empty();
+      // if (users.length !== 0) {
+      //   users.forEach(function(user) {
+      //     appendUser(user);
+      //   });
+      // }
+      // else {
+      //   appendNoUserToHTML("一致するユーザーは見つかりません");
+      // }
+    })
+    .fail(function() {
+      alert('検索に失敗しました');
+    })
+  });
+});
